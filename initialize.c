5d8b130d437e72d09b9752c185da54b299821aea
julcalde <46924189+julcalde@users.noreply.github.com>
2025-05-02 13:30:43 +0200

    added ft_atoi to substitute atoi

--- initialize.c
+++ initialize.c
@@ -14,12 +14,12 @@
 
 void	init_data(t_data *data, int argc, char **argv)
 {
-	data->num_philos = atoi(argv[1]);
-	data->time_to_die = atoi(argv[2]);
-	data->time_to_eat = atoi(argv[3]);
-	data->time_to_sleep = atoi(argv[4]);
+	data->num_philos = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-		data->required_meals = atoi(argv[5]);
+		data->required_meals = ft_atoi(argv[5]);
 	else
 		data->required_meals = -1;
 	data->is_dead = 0;
@@ -31,4 +31,32 @@ void	init_data(t_data *data, int argc, char **argv)
 	}
 	init_mutexes(data);
 	data->start_time = get_time_msec();
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] >= 9 && (str[i] <= 13 || str[i] == 32))
+		i++;
+	if (str[i] == '+' && str[i + 1] != '-')
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] && ft_isdigit(str[i]))
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
 }
