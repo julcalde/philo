fd41b1d6cb5055afea12713c25606018f8de2fa8
julcalde <julcalde@student.42heilbronn.de>
2025-05-04 20:30:38 +0200

    test 4 410 200 200 ahhhh

--- forks.c
+++ forks.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 14:25:27 by julcalde          #+#    #+#             */
-/*   Updated: 2025/05/04 19:53:18 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/05/04 20:26:48 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,19 @@ int	take_first_fork(t_philo *philo, int *first, int *second)
 	*second = (philo->id + 1) % data->num_philos;
 	if (data->num_philos == 1)
 	{
+		if (check_death(philo))
+			return (1);
 		pthread_mutex_lock(&data->forks[*first]);
 		print_status(philo, "has taken a fork");
 		return (1);
 	}
 	if (philo->id % 2 == 0)
 	{
-		if (acquire_even_first(philo, first, second))
-			return (0);
+		acquire_even_first(philo, first, second);
 	}
 	else
 	{
-		if (acquire_odd_first(philo, first, second))
-			return (0);
+		acquire_odd_first(philo, first, second);
 	}
 	return (0);
 }
@@ -54,13 +54,11 @@ int	take_second_fork(t_philo *philo, int first, int second)
 {
 	if (philo->id % 2 == 0)
 	{
-		if (acquire_even_second(philo, first, second))
-			return (1);
+		acquire_even_second(philo, first, second);
 	}
 	else
 	{
-		if (acquire_odd_second(philo, first, second))
-			return (1);
+		acquire_odd_second(philo, first, second);
 	}
 	print_status(philo, "has taken a fork");
 	return (0);
@@ -69,17 +67,17 @@ int	take_second_fork(t_philo *philo, int first, int second)
 void	release_forks(t_philo *philo)
 {
 	t_data	*data;
-	int		left;
-	int		right;
+	int		first;
+	int		second;
 
 	data = philo->data;
-	left = philo->id;
-	right = (philo->id + 1) % data->num_philos;
+	first = philo->id;
+	second = (philo->id + 1) % data->num_philos;
 	if (data->num_philos == 1)
-		pthread_mutex_unlock(&data->forks[left]);
+		pthread_mutex_unlock(&data->forks[first]);
 	else
 	{
-		pthread_mutex_unlock(&data->forks[left]);
-		pthread_mutex_unlock(&data->forks[right]);
+		pthread_mutex_unlock(&data->forks[first]);
+		pthread_mutex_unlock(&data->forks[second]);
 	}
 }
