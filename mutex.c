3438b82205fcc0d7ab35cc17cf059605b51e1325
Julio Max Calderone Da Silva <julcalde@2-F-5.42heilbronn.de>
2025-05-06 21:31:01 +0200

    mutex init protects

--- mutex.c
+++ mutex.c
@@ -6,24 +6,35 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 15:18:26 by julcalde          #+#    #+#             */
-/*   Updated: 2025/05/04 14:26:13 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/05/06 21:23:20 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_mutexes(t_data *data)
+int	init_mutexes(t_data *data)
 {
 	int	i;
 
 	i = 0;
+	if (pthread_mutex_init(&data->print_mutex, NULL))
+		return (-1);
+	if (pthread_mutex_init(&data->dead_mutex, NULL))
+	{
+		pthread_mutex_destroy(&data->print_mutex);
+		return (-1);
+	}
 	while (i < data->num_philos)
 	{
-		pthread_mutex_init(&data->forks[i], NULL);
+		if (pthread_mutex_init(&data->forks[i], NULL))
+		{
+			while (--i)
+				pthread_mutex_destroy(&data->forks[i]);
+			return (-1);
+		}
 		i++;
 	}
-	pthread_mutex_init(&data->print_mutex, NULL);
-	pthread_mutex_init(&data->dead_mutex, NULL);
+	return (0);
 }
 
 void	destroy_mutexes(t_data *data)
