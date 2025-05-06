d9ff69af0ac764f3e1755b44712ef7c31aed7cfe
Julio Max Calderone Da Silva <julcalde@2-F-5.42heilbronn.de>
2025-05-06 23:28:16 +0200

    PROGRESS! Monitor is helping out, but there are some cases to fix.

--- actions.c
+++ actions.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 20:45:54 by julcalde          #+#    #+#             */
-/*   Updated: 2025/05/06 22:01:33 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/05/06 23:25:57 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,14 @@ int	routine_eat(t_philo *philo)
 	t_data	*data;
 
 	data = philo->data;
+	if (get_is_dead(data))
+		return (0);
 	take_forks(philo);
+	if (get_is_dead(data))
+	{
+		release_forks(philo);
+		return (0);
+	}
 	philo->last_meal_time = get_time_msec();
 	philo->meals_eaten++;
 	print_status(philo, "is eating");
@@ -31,7 +38,7 @@ int	routine_eat(t_philo *philo)
 int	routine_think(t_philo *philo)
 {
 	print_status(philo, "is thinking");
-	ft_usleep(10, philo);
+	ft_usleep(1, philo);
 	if (check_death(philo))
 		return (0);
 	return (1);
--- main.c
+++ main.c
@@ -6,33 +6,83 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 14:29:19 by julcalde          #+#    #+#             */
-/*   Updated: 2025/05/06 21:32:01 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/05/06 23:20:44 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+void	*monitor(void *arg)
 {
-	t_data		data ;
-	pthread_t	threads[200];
-	t_philo		philos[200];
-	int			i;
+	t_philo	*philos;
+	t_data	*data;
+	int		i;
+	int		done;
+
+	philos = (t_philo *)arg;
+	data = philos[0].data;
+	while (!get_is_dead(data))
+	{
+		if (data->required_meals != -1)
+		{
+			done = 1;
+			i = 0;
+			while (i < data->num_philos)
+			{
+				if (philos[i].meals_eaten < data->required_meals)
+					done = 0;
+				i++;
+			}
+			if (done)
+				return (NULL);
+		}
+		i = 0;
+		while (i < data->num_philos)
+		{
+			if (check_death(&philos[i]))
+				return (NULL);
+			i++;
+		}
+		usleep(1000);
+	}
+	return (NULL);
+}
+
+void	init_ph_and_th(t_data *data, t_philo *philos, pthread_t *threads)
+{
+	int	i;
 
 	i = 0;
-	if (argc < 5 || argc > 6)
-		return (printf (USAGE_MSG), EXIT_FAILURE);
-	init_data(&data, argc, argv);
-	while (i < data.num_philos)
+	while (i < data->num_philos)
 	{
 		philos[i].id = i;
-		philos[i].data = &data;
-		philos[i].last_meal_time = data.start_time - data.time_to_eat;
+		philos[i].data = data;
+		philos[i].last_meal_time = data->start_time - data->time_to_eat;
 		philos[i].meals_eaten = 0;
 		i++;
 	}
-	create_threads(&data, philos, threads);
-	join_threads(&data, threads);
+	create_threads(data, philos, threads);
+	i = 0;
+	while (i < data->num_philos)
+	{
+		pthread_detach(threads[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_data		data;
+	pthread_t	threads[200];
+	pthread_t	monitor_thread;
+	t_philo		philos[200];
+
+	if (argc < 5 || argc > 6)
+		return (printf(USAGE_MSG), EXIT_FAILURE);
+	init_data(&data, argc, argv);
+	init_ph_and_th(&data, philos, threads);
+	pthread_create(&monitor_thread, NULL, monitor, philos);
+	pthread_join(monitor_thread, NULL);
 	destroy_mutexes(&data);
 	free(data.forks);
 	return (0);
