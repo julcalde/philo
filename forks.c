fc487ca1f4c0033431eea9a56316b1358ee613d7
Julio Max Calderone Da Silva <julcalde@2-C-7.42heilbronn.de>
2025-05-10 22:09:25 +0200

    found the source of all evils

--- forks.c
+++ forks.c
@@ -6,11 +6,59 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 14:25:27 by julcalde          #+#    #+#             */
-/*   Updated: 2025/05/10 21:51:58 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/05/10 22:09:02 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	even_ph_id(t_philo *philo)
+{
+	int	left;
+	int	right;
+
+	left = philo->id;
+	right = (philo->id + 1) % philo->data->num_philos;
+	pthread_mutex_lock(&philo->data->forks[right]);
+	if (get_is_dead(philo->data))
+	{
+		pthread_mutex_unlock(&philo->data->forks[right]);
+		return ;
+	}
+	print_status(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->data->forks[left]);
+	if (get_is_dead(philo->data))
+	{
+		pthread_mutex_unlock(&philo->data->forks[right]);
+		pthread_mutex_unlock(&philo->data->forks[left]);
+		return ;
+	}
+	print_status(philo, "has taken a fork");
+}
+
+void	odd_ph_id(t_philo *philo)
+{
+	int	left;
+	int	right;
+
+	left = philo->id;
+	right = (philo->id + 1) % philo->data->num_philos;
+	pthread_mutex_lock(&philo->data->forks[left]); // here is the deadlock
+	if (get_is_dead(philo->data))
+	{
+		pthread_mutex_unlock(&philo->data->forks[left]);
+		return ;
+	}
+	print_status(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->data->forks[right]);
+	if (get_is_dead(philo->data))
+	{
+		pthread_mutex_unlock(&philo->data->forks[left]);
+		pthread_mutex_unlock(&philo->data->forks[right]);
+		return ;
+	}
+	print_status(philo, "has taken a fork");
+}
 
 void	take_forks(t_philo *philo)
 {
@@ -23,17 +71,11 @@ void	take_forks(t_philo *philo)
 		return ;
 	if (philo->id % 2 == 0)
 	{
-		pthread_mutex_lock(&philo->data->forks[right]);
-		print_status(philo, "has taken a fork");
-		pthread_mutex_lock(&philo->data->forks[left]);
-		print_status(philo, "has taken a fork");
+		even_ph_id(philo);
 	}
 	else
 	{
-		pthread_mutex_lock(&philo->data->forks[left]); // deadlock occurs here for test case "4 310 200 100"
-		print_status(philo, "has taken a fork");
-		pthread_mutex_lock(&philo->data->forks[right]);
-		print_status(philo, "has taken a fork");
+		odd_ph_id(philo);
 	}
 	if (check_death(philo))
 	{
